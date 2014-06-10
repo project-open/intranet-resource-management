f79359684d78b278aa604510d6f6a0790a8251e2
Frank Bergmann <frank.bergmann@project-open.com>
2014-06-10 16:09:21 +0000

    - Added assignments function

--- sql/postgresql/intranet-resource-management-create.sql
+++ sql/postgresql/intranet-resource-management-create.sql
@@ -259,8 +259,7 @@ end;'
 
 -- Returns a real[] for each day between start and end 
 -- with 100 for working days and 0 for weekends
-drop function im_rm_weekend(integer, date, date);
-create or replace function im_rm_weekend (integer, date, date)
+create or replace function im_resource_mgmt_weekend (integer, date, date)
 returns float[] as $body$
 DECLARE
 	p_user_id			alias for $1;
@@ -285,14 +284,13 @@ BEGIN
 
 	return v_work_days;
 END;$body$ language 'plpgsql';
-select im_rm_weekend(624, now()::date, '2014-06-30');
+-- select im_resource_mgmt_weekend(624, now()::date, '2014-06-30');
 
 
 
 -- Returns a real[] for each day between start and end 
 -- with 100 for working days and 0 for weekend + bank holidays
-drop function im_rm_work_days(integer, date, date);
-create or replace function im_rm_work_days (integer, date, date)
+create or replace function im_resource_mgmt_work_days (integer, date, date)
 returns float[] as $body$
 DECLARE
 	p_user_id			alias for $1;
@@ -304,7 +302,7 @@ DECLARE
 	v_work_days			float[];
 	row				record;
 BEGIN
-	v_work_days = im_rm_weekend(p_user_id, p_start_date, p_end_date);
+	v_work_days = im_resource_mgmt_weekend(p_user_id, p_start_date, p_end_date);
 
 	-- Apply "Bank Holiday" absences
 	FOR row IN
@@ -316,7 +314,7 @@ BEGIN
 			a.absence_type_id = 5005 and -- bank holidays
 			a.absence_status_id in (select child_id from im_category_hierarchy where parent_id = 16000 UNION select 16000) -- active
 	LOOP
-		RAISE NOTICE 'im_rm_user_absence(%,%,%): Bank Holiday %', p_user_id, p_start_date, p_end_date, row.absence_name;
+		RAISE NOTICE 'im_resource_mgmt_user_absence(%,%,%): Bank Holiday %', p_user_id, p_start_date, p_end_date, row.absence_name;
 		v_date := row.start_date;
 		WHILE (v_date <= row.end_date) LOOP
 			v_work_days[v_date - p_start_date] := 0;
@@ -326,14 +324,13 @@ BEGIN
 
 	return v_work_days;
 END;$body$ language 'plpgsql';
-select im_rm_work_days(624, now()::date, '2014-06-30');
+-- select im_resource_mgmt_work_days(624, now()::date, '2014-06-30');
 
 
 -- Returns a real[] for each day between start and end 
 -- with 100 for each day the user has taken vacation.
 -- Half days off will appear with 50.
-drop function im_rm_user_absence(integer, date, date);
-create or replace function im_rm_user_absence (integer, date, date)
+create or replace function im_resource_mgmt_user_absence (integer, date, date)
 returns float[] as $body$
 DECLARE
 	p_user_id			alias for $1;
@@ -348,7 +345,7 @@ DECLARE
 	v_absence_percentage		float;
 	row				record;
 BEGIN
-	v_work_days = im_rm_work_days(p_user_id, p_start_date, p_end_date);
+	v_work_days = im_resource_mgmt_work_days(p_user_id, p_start_date, p_end_date);
 
 	-- Initiate the result array with weekends
 	v_date := p_start_date;
@@ -383,17 +380,17 @@ BEGIN
 		ELSE
 			-- Error: Division by zero:
 			-- The vacation is completely within bank holidays.
-			RAISE WARNING 'im_rm_user_absence(%,%,%): Absence %: No workdays for vacation', p_user_id, p_start_date, p_end_date, row.absence_name;
+			RAISE WARNING 'im_resource_mgmt_user_absence(%,%,%): Absence %: No workdays for vacation', p_user_id, p_start_date, p_end_date, row.absence_name;
 			v_absence_percentage := 0;
 		END IF;
 		IF v_absence_duration_work_days > 100 THEN
 			-- Error: Overlapping vacations or duration days > work days
-			RAISE WARNING 'im_rm_user_absence(%,%,%): Absence %: More duration_days=% than work_days=%', p_user_id, p_start_date, p_end_date, row.absence_name, row.duration_days, v_absence_duration_work_days;
+			RAISE WARNING 'im_resource_mgmt_user_absence(%,%,%): Absence %: More duration_days=% than work_days=%', p_user_id, p_start_date, p_end_date, row.absence_name, row.duration_days, v_absence_duration_work_days;
 			v_absence_percentage := 100;
 		END IF;
 
 		-- Add the absence percentage to the result set
-		RAISE NOTICE 'im_rm_user_absence(%,%,%): Absence %: dur=%, workdays=%, perc=%', p_user_id, p_start_date, p_end_date, row.absence_name, row.duration_days, v_absence_duration_work_days, v_absence_percentage;
+		RAISE NOTICE 'im_resource_mgmt_user_absence(%,%,%): Absence %: dur=%, workdays=%, perc=%', p_user_id, p_start_date, p_end_date, row.absence_name, row.duration_days, v_absence_duration_work_days, v_absence_percentage;
 
 		-- Add the vacation to the vacation days
 		v_date := row.start_date;
@@ -407,40 +404,64 @@ BEGIN
 
 	return v_result;
 END;$body$ language 'plpgsql';
-select im_rm_user_absence(624, now()::date, '2014-06-30');
+-- select im_resource_mgmt_user_absence(624, now()::date, '2014-06-30');
 
 
 
 
 -- Returns a real[] for each day between start and end 
 -- with 100% if a user is fully assigned to a project.
-drop function im_rm_user_assignation(integer, date, date);
-create or replace function im_rm_user_assignation (integer, date, date)
-returns float[] as $body$
+CREATE OR REPLACE FUNCTION im_resource_mgmt_project_assignation(integer, date, date, boolean)
+  RETURNS double precision[] AS
+$BODY$
 DECLARE
-	p_user_id			alias for $1;
-	p_start_date			alias for $2;
-	p_end_date			alias for $3;
+	p_user_id		alias for $1;
+	p_start_date		alias for $2;
+	p_end_date		alias for $3;
+	p_productive_p		alias for $4;
 
-	v_date				date;
-	v_work_days			float[];
-	v_result			float[];
-	row				record;
+	v_date			date;
+	v_work_days		float[];
+	v_result		float[];
+	row			record;
 BEGIN
-	v_work_days = im_rm_work_days(p_user_id, p_start_date, p_end_date);
+	v_work_days = im_resource_mgmt_work_days(p_user_id, p_start_date, p_end_date);
 
-	FOR row 
-	select	*
-	from	im_projects p,
-		acs_rels r,
-		im_biz_object_members bom
-	where	r.object_id_one = p.project_id and
-		r.object_id_two = 624 and
-		r.rel_id = bom.rel_id and
-		bom.percentage > 0
+	-- Initiate the result array with weekends
+	v_date := p_start_date;
+	WHILE (v_date <= p_end_date) LOOP
+		v_result[v_date - p_start_date] := 0;
+		v_date := v_date + 1;
+	END LOOP;
 
+	FOR row IN
+		select	*,
+			CASE WHEN c.company_path = 'internal' THEN false ELSE true END as project_productive_p
+		from	im_projects p,
+			im_companies c,
+			acs_rels r,
+			im_biz_object_members bom
+		where	p.company_id = c.company_id and
+			r.object_id_one = p.project_id and
+			r.rel_id = bom.rel_id and
+			(r.object_id_two = p_user_id OR r.object_id_two in (select group_id from group_distinct_member_map where member_id = p_user_id)) and
+			bom.percentage is not null and
+			bom.percentage > 0 and
+			p.end_date >= p_start_date and
+			p.start_date <= p_end_date
+	LOOP
+		RAISE NOTICE 'im_resource_mgmt_project_assignation(%,%,%): Project=%: perc=%', p_user_id, p_start_date, p_end_date, row.project_name, row.percentage;
+		IF p_productive_p is NULL OR (p_productive_p = row.project_productive_p) THEN
+			v_date := row.start_date;
+			IF v_date < p_start_date THEN v_date := p_start_date; END IF;
+	
+			WHILE (v_date <= row.end_date and v_date <= p_end_date) LOOP
+				v_result[v_date - p_start_date] := row.percentage + v_result[v_date - p_start_date];
+				v_date := v_date + 1;
+			END LOOP;
+		END IF;
+	END LOOP;
 	return v_result;
-END;$body$ language 'plpgsql';
-select im_rm_user_absence(624, now()::date, '2014-06-30');
-
+END;$BODY$ language 'plpgsql';
+-- select im_resource_mgmt_user_absence(624, now()::date, '2014-06-30', null);
 
